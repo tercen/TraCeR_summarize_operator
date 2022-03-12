9d5e177011a62642eb33478a4719c020dd46f6e2
fcadete <filipe@cadete.eu>
2022-03-12 18:45:44 +0000

    Takes input folder from tercen

--- main.R
+++ main.R
@@ -6,18 +6,19 @@ library(stringr)
 
 ctx <- tercenCtx()
 
+input_folder <- ctx$cselect()[[1]][[1]]
 
-# Define output path
-output_path <- "/var/lib/tercen/share/write/tracer_output"
+# Define input and output paths
+input_path <- paste0("/var/lib/tercen/share/write/", input_folder)
 
 # Check if a "tracer_output" folder exists and is not empty
-if( dir.exists(output_path) == FALSE) {
+if( dir.exists(input_path) == FALSE) {
 
   stop("ERROR: tracer_output folder does not exist in project write folder.")
 
 }
 
-if (length(dir(output_path)) == 0) {
+if (length(dir(input_path)) == 0) {
   stop("ERROR: tracer_output folder is empty.")
 }
 
@@ -29,20 +30,20 @@ args <- paste('summarise',
               '--ncores', parallel::detectCores(),
               '--config_file /tercen_tracer.conf',
               '-s Hsap',
-              output_path,
+              input_path,
               sep = ' ')
 
 system2(cmd, args)
 
 # run the collect script
 
-cmd <- paste0("python /collect_TRA_TRB_in_fasta.py ", output_path, "/*/filtered_TCR_seqs/*.fa > ", output_path, "/summarise_output.tsv")
+cmd <- paste0("python /collect_TRA_TRB_in_fasta.py ", input_path, "/*/filtered_TCR_seqs/*.fa > ", input_path, "/summarise_output.tsv")
 
 system(cmd)
 
-collected_summary <- read_tsv(paste0(output_path, "/summarise_output.tsv"))
+collected_summary <- read_tsv(paste0(input_path, "/summarise_output.tsv"))
 
-recombinants <- read_tsv(paste0(output_path, "/filtered_TCRAB_summary/recombinants.txt"))
+recombinants <- read_tsv(paste0(input_path, "/filtered_TCRAB_summary/recombinants.txt"))
 
 collected_summary <- left_join(collected_summary,
                                recombinants,
